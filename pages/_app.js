de033a12a3f66295089dc2b013cbac665b8d52ff
azat valiev <azvaliev@gmail.com>
2022-03-27 14:20:40 -0700

    Added favicon & og tags

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,6 @@ function MyApp({ Component, pageProps }) {
   return (
   <>
     <Head>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta charSet="utf-8" />
       <title>Azat Valiev</title>
       <meta 
         name="description" 
@@ -16,10 +14,7 @@ function MyApp({ Component, pageProps }) {
           Are you looking for a skilled Web Developer to build/improve your websites or applications?
           My name is Azat, and I've been doing just that for individuals and businesses for 4+ years
         `}/>
-      <link rel="icon" type="image/png" sizes="16x16" href='/img/logo.png' />
-      <link rel="preconnect" href="https://fonts.googleapis.com"/> 
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/> 
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500&family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet"/>
+
     </Head>
     <div className="app" >
       <Navbar/>
