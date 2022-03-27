9d912c7004227d8290f190f7888a4ee4828ff7c2
azat valiev <azvaliev@gmail.com>
2022-03-26 19:39:23 -0700

    fixed title and description

--- pages/_app.js
+++ pages/_app.js
@@ -9,8 +9,13 @@ function MyApp({ Component, pageProps }) {
     <Head>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta charSet="utf-8" />
-      <title>Enhanced CEO</title>
-      <meta name="description" content="Are you an entrepreneur looking to optimize productivity and get more out of your time? If so, look no further"/>
+      <title>Azat Valiev</title>
+      <meta 
+        name="description" 
+        content={`
+          Are you looking for a skilled Web Developer to build/improve your websites or applications?
+          My name is Azat, and I've been doing just that for individuals and businesses for 4+ years
+        `}/>
       <link rel="icon" type="image/png" sizes="16x16" href='/img/logo.png' />
       <link rel="preconnect" href="https://fonts.googleapis.com"/> 
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/> 
